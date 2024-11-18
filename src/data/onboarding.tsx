2d1442ff727408791a7d1a0500a3fd74e9cde050
muhdlubega <111652389+muhdlubega@users.noreply.github.com>
2024-11-18 03:15:43 +0000

    fix: archive onboarding

--- src/data/onboarding.tsx
+++ src/data/onboarding.tsx
@@ -60,7 +60,7 @@ const TourContent = ({ children, isLastStep = false }: {children: JSX.Element, i
   );
 };
 
-export const getSteps = (hasStats: boolean) => {
+export const getSteps = (hasStats: boolean, screenshots: string[]) => {
   const baseSteps = [
     {
       selector: ".onboarding01",
@@ -105,96 +105,36 @@ export const getSteps = (hasStats: boolean) => {
     },
   ];
 
+  const hasArchives = (screenshots: string[]) => {
+    return screenshots[0]?.split("/")[0] !== '1';
+  };
+
   if (hasStats) {
-    baseSteps.push(
-      {
-        selector: ".onboarding04",
-        content: () => (
-          <TourContent>
-            <div>
-              <p>Click on this button to check and share your stats.</p>
-              <p>
-                <strong>Disclaimer:</strong> Stats are stored in your cookies and will be reset if cookies are cleared
-              </p>
-            </div>
-          </TourContent>
-        )
-      },
-      {
+    baseSteps.push({
+      selector: ".onboarding04",
+      content: () => (
+        <TourContent>
+          <div>
+            <p>Click on this button to check and share your stats.</p>
+            <p>
+              <strong>Disclaimer:</strong> Stats are stored in your cookies and will be reset if cookies are cleared
+            </p>
+          </div>
+        </TourContent>
+      )
+    });
+
+    if (hasArchives(screenshots)) {
+      baseSteps.push({
         selector: ".onboarding05",
         content: () => (
           <TourContent isLastStep={true}>
             <p>To check and try out previous games check the <strong>Archives</strong> here</p>
           </TourContent>
         )
-      }
-    );
+      });
+    }
   }
 
   return baseSteps;
 };
-
-// export const getSteps = (hasStats: boolean) => {
-//   const baseSteps = [
-//     {
-//       selector: ".onboarding01",
-//       content: () => (
-//         <div>
-//           <h2 style={{
-//             color: '#FF2247',
-//             fontSize: '24px',
-//             marginBottom: '10px',
-//             fontWeight: 'bold',
-//             fontFamily: 'Lobster'
-//           }}>
-//             Muizzle
-//           </h2>
-//           <p>
-//             Welcome to Muizzle! Wordle Tamil movie guesser.
-//             Everyday at 4pm MYT a new movie will be displayed
-//           </p>
-//         </div>
-//       )
-//     },
-//     {
-//       selector: ".onboarding02",
-//       content: () => (
-//         <p>You have <strong>6 attempts</strong> to guess the title of the movie based on the screenshots displayed</p>
-//       )
-//     },
-//     {
-//       selector: ".onboarding03",
-//       content:
-//         () => (
-//           <div>
-//             <p>Input your guess and select an option</p>
-//             <p>On winning or losing your first game your statistics modal will be displayed</p>
-//           </div>
-//         )
-//     },
-//   ];
-
-//   if (hasStats) {
-//     baseSteps.push(
-//       {
-//         selector: ".onboarding04",
-//         content: () => (
-//           <div>
-//             <p>Click on this button to check and share your stats.</p>
-//             <p>
-//               <strong>Disclaimer:</strong> Stats are stored in your cookies and will be reset if cookies are cleared
-//             </p>
-//           </div>
-//         )
-//       },
-//       {
-//         selector: ".onboarding05",
-//         content: () => (
-//           <p>To check and try out previous games check the <strong>Archives</strong> here</p>
-//         )
-//       }
-//     );
-//   }
-
-//   return baseSteps;
-// };
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { TourProvider } from "@reactour/tour";
 import { getSteps, tourConfig } from './data/onboarding.tsx';
+import { files } from './data/screenshots';
 import Cookies from 'js-cookie';
 import App from './App.tsx'
 import './index.css'
@@ -10,7 +11,7 @@ const hasStats = Boolean(Cookies.get("gamesPlayed"));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TourProvider steps={getSteps(hasStats)}
+    <TourProvider steps={getSteps(hasStats, files)}
       styles={tourConfig.styles}
       showNavigation={true}
       position="bottom" >
